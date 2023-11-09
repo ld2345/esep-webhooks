670016eb581b3adc436f34e3cf6ef0b909249e93
ld2345 <80736207+ld2345@users.noreply.github.com>
2023-11-09 16:45:40 -0500

    Update function.py

--- function.py
+++ function.py
@@ -3,34 +3,23 @@ import os
 import requests
 
 def lambda_handler(event, context):
-    # Verify the GitHub webhook event type
-    headers = event['headers']
-    if 'X-GitHub-Event' in headers and headers['X-GitHub-Event'] == 'issues':
-        # Deserialize the incoming JSON payload
-        input_json = json.loads(event['body'])
-        issue_url = input_json['issue']['html_url']
+    # Assuming event is a JSON object
+    input_data = json.loads(event)
 
-        # Prepare Slack message payload
-        slack_payload = {
-            'text': f'Issue Created: {issue_url}'
-        }
+    # Extract the URL of the created issue
+    issue_url = input_data.get("issue", {}).get("html_url", "")
 
-        # Send the message to Slack
-        response = requests.post(os.environ['SLACK_URL'], json=slack_payload)
+    # Construct the payload for Slack
+    payload = {
+        "text": f"Issue Created: {issue_url}"
+    }
 
-        # Check the response from Slack
-        if response.status_code == 200:
-            return {
-                'statusCode': 200,
-                'body': json.dumps('Message sent successfully to Slack!')
-            }
-        else:
-            return {
-                'statusCode': response.status_code,
-                'body': json.dumps('Failed to send message to Slack!')
-            }
-    else:
-        return {
-            'statusCode': 400,
-            'body': json.dumps('Invalid GitHub webhook event!')
-        }
+    # Send data to Slack using webhook URL
+    slack_url = os.environ.get("SLACK_URL")
+    response = requests.post(slack_url, json=payload)
+
+    # Return the response from Slack as the result of the Lambda function
+    return {
+        "statusCode": response.status_code,
+        "body": response.text
+    }
